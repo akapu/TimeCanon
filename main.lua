84f3fe5b5a989fc15fb158538d7564befbc428aa
akapu <alexkap544@gmail.com>
2023-02-09 08:02:17 +1000

    add control on left/right keys

--- main.lua
+++ main.lua
@@ -1,25 +1,14 @@
 W_WIDTH, W_HEIGHT = love.graphics.getDimensions()
 WIDTH = 75
 HEIGHT = 25
+ROTATION_SPEED = math.pi / 6
 
 function love.load()
     love.window.setTitle('TimeCanon')
 
     love.keyboard.keysPressed = {}
-    love.mouse.isPressed = nil
-    
-end
 
-function love.mousepressed(x, y, button, istouch)
-    love.mouse.isPressed = {
-        x = x - W_WIDTH/2,
-        y = y - W_HEIGHT/2,
-        isTouch = istouch
-    }
-end
-
-function love.mousereleased()
-    love.mouse.isPressed = nil
+    angle = 0
 end
 
 function love.keypressed(key)
@@ -30,34 +19,29 @@ function love.keypressed(key)
     love.keyboard.keysPressed[key] = true
 end
 
+function love.keyreleased(key)
+    love.keyboard.keysPressed[key] = false
+end
+
 function love.update(dt)
-    love.keyboard.keysPressed = {}
+    local current_speed = 0
 
-    initialPos = love.mouse.isPressed
-
-    if initialPos then
-        pos = {
-            x = love.mouse.getX() - W_WIDTH/2,
-            y = love.mouse.getY() - W_HEIGHT/2
-        }
-
-        local dotProduct = initialPos.x * pos.x + initialPos.y * pos.y
-        local initialPosLength = math.sqrt(initialPos.x ^ 2 + initialPos.y ^ 2)
-        local posLength = math.sqrt(pos.x ^ 2 + pos.y ^ 2)
-
-        angle = math.acos(dotProduct / (initialPosLength * posLength))
-
+    if love.keyboard.keysPressed['left'] then
+        current_speed = current_speed - ROTATION_SPEED
     end
 
+    if love.keyboard.keysPressed['right'] then
+        current_speed = current_speed + ROTATION_SPEED
+    end
+
+    angle = angle + current_speed * dt
 end
 
 function love.draw()
     love.graphics.push()
 	love.graphics.translate(W_WIDTH/2, W_HEIGHT/2)
 
-    if initialPos then
-    	love.graphics.rotate(angle)
-    end
+    love.graphics.rotate(angle)
 
 	love.graphics.rectangle('fill', -WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT)
 	love.graphics.pop()
