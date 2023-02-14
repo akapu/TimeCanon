5e067016b3e8a6995032e457e706fde8d8cf5df3
akapu <alexkap544@gmail.com>
2023-02-15 08:41:13 +1000

    add bullets

--- main.lua
+++ main.lua
@@ -3,12 +3,20 @@ WIDTH = 75
 HEIGHT = 25
 ROTATION_SPEED = math.pi / 6
 
+BULLET_SIZE = 24
+BULLET_SPEED = 30
+BULLET_PERIOD = 1
+
 function love.load()
     love.window.setTitle('TimeCanon')
 
     love.keyboard.keysPressed = {}
 
     angle = 0
+
+    timer = 0
+
+    bullets = {}
 end
 
 function love.keypressed(key)
@@ -24,6 +32,24 @@ function love.keyreleased(key)
 end
 
 function love.update(dt)
+    timer = timer + dt
+
+    if timer > BULLET_PERIOD then
+        bullets[#bullets + 1] = {
+            pos = {x = W_WIDTH/2, y = W_HEIGHT/2},
+            dir = {
+                x = math.cos(angle),
+                y = math.sin(angle)
+            }}
+        
+        timer = 0
+    end 
+
+    for _, bullet in pairs(bullets) do
+        bullet.pos.x = bullet.pos.x + bullet.dir.x * BULLET_SPEED * dt
+        bullet.pos.y = bullet.pos.y + bullet.dir.y * BULLET_SPEED * dt
+    end
+
     local current_speed = 0
 
     if love.keyboard.keysPressed['left'] then
@@ -38,6 +64,10 @@ function love.update(dt)
 end
 
 function love.draw()
+    for _, bullet in pairs(bullets) do
+        love.graphics.circle('fill', bullet.pos.x, bullet.pos.y, BULLET_SIZE)
+    end
+
     love.graphics.push()
 	love.graphics.translate(W_WIDTH/2, W_HEIGHT/2)
 
